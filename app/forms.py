5bb1dd4190cdcacabc7f75c207a0faf5b484f9ce
Mike Oberdick <30874065+Obe2121@users.noreply.github.com>
2021-10-28 17:41:31 -0400

    day 3 hw update

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Email, DataRequired
+from wtforms.validators import Email, DataRequired, EqualTo,ValidationError
+from .models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email Address', validators=[DataRequired(),Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class PokemonForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+
+class RegisterForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
+from flask.helpers import url_for
 import requests
 from app import app
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm, PokemonForm
+from .models import User
+from flask_login import login_user, current_user, logout_user, login_required
+
 @app.route('/', methods = ['GET'])
 def index():
     return render_template('index.html.j2')
@@ -10,21 +14,50 @@ def index():
 def login():
     form = LoginForm()
     if request.method == 'POST' and form.validate_on_submit():
-        #do Login stuff
         email = request.form.get("email").lower()
         password = request.form.get("password")
-        if email in app.config.get("REGISTERED_USERS") and \
-            password == app.config.get("REGISTERED_USERS").get(email).get('password'):
-            return f"Login success Welcome {app.config.get('REGISTERED_USERS').get(email).get('name')}"
+        u = User.query.filter_by(email=email).first()
+
+        if u and u.check_hashed_password(password):
+            login_user(u)
+            return redirect(url_for("index"))
         error_string = "Invalid Email password combo"
         return render_template('login.html.j2', error = error_string, form=form)
-
     return render_template('login.html.j2', form=form)
 
+@app.route('/logout')
+@login_required
+def logout():
+    if current_user:
+        logout_user()
+        return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        try:
+            new_user_data = {
+                "first_name":form.first_name.data.title(),
+                "last_name":form.last_name.data.title(),
+                "email":form.email.data.lower(),
+                "password":form.password.data,
+            }
+            new_user_object = User()
+            new_user_object.from_dict(new_user_data)
+            new_user_object.save()
+        except:
+            error_string = 'There was an unexpected error creating your account. Please try again later'
+            return render_template('register.html.j2', form=form, error = error_string)
+        return redirect(url_for('login'))
+    return render_template('register.html.j2', form = form)
+
 @app.route('/pokemon', methods = ['GET', 'POST'])
+@login_required
 def pokemon():
-    if request.method == 'POST':
-        name = request.form.get('pokemon_name')
+    form = PokemonForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        name = request.form.get('name')
         url = f'https://pokeapi.co/api/v2/pokemon/{name}/'
         response = requests.get(url)
         if response.ok:
